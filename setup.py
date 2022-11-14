288c264a9f96f0b9b33a7ab0fac8829fb81c6305
Nima Boscarino <nima@huggingface.co>
2022-11-14 10:47:56 -0800

    Set version

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name="disaggregators",
-    version="0.1.1",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
+    version="0.1.0.dev0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
     description="HuggingFace community-driven open-source library for dataset disaggregation",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
