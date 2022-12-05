937cb543fcd2190c50bca1ea6d586d94d376340d
Nima Boscarino <nima@huggingface.co>
2022-12-05 13:24:29 -0800

    Use nox for testing multiple python versions

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+import nox
+
+python_versions = ["3.8", "3.9", "3.10"]
+
+
+@nox.session(python=python_versions)
+def tests(session):
+    session.install('.[tests]')
+    session.run("spacy", "download", "en_core_web_lg")
+    session.run('pytest')
