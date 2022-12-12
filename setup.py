ef5401dd45da0309f6c929466ff3b189e38adf85
Nima Boscarino <nima@huggingface.co>
2022-12-12 08:59:49 -0800

    set dev version

--- setup.py
+++ setup.py
@@ -60,8 +60,8 @@ REQUIRED_PKGS = [
     "packaging",
     "spacy",
     "datasets",
-    "aenum>=3.1",
-    "sentence-transformers",
+    "aenum>=3.1.11",
+    "sentence-transformers>=2.2.2",
     "geograpy3",
     "nltk",
     "requests",
@@ -88,7 +88,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name="disaggregators",
-    version="0.1.2",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
+    version="0.1.3.dev0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
     description="HuggingFace community-driven open-source library for dataset disaggregation",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
